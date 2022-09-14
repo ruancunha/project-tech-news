d3f2a0cfa527ab4497e6573849a3f85683e5bc08
RuanCunha <ruanvcunha@hotmail.com>
2022-09-14 02:02:52 -0300

    feat: 🎸 implement search_by_title

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,15 @@
+from operator import itemgetter
+from tech_news.database import search_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = search_news({})
+    result = sorted(news, key=itemgetter('comments_count'), reverse=True)
+    if len(result) > 5:
+        result = result[:5]
+
+    return [(article["title"], article["url"])for article in result]
 
 
 # Requisito 11
